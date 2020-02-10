b0c9f4e33358a984ab80ed5c7061808358bae030
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-02-10 19:35:24 +0100

    Commit

--- App.cpp
+++ App.cpp
@@ -20,18 +20,18 @@ int main()
 
 		console.Clear();
 
-		console.DrawLine(10.0f, 61.0f, 80.0f, 15.0f);
-		console.DrawLine(10.0f, 61.0f, 80.0f, 61.0f);
-		console.DrawLine(0.0f, 0.0f, 3.0f, 4.0f);
+		//console.DrawLine(10.1f, 61.2f, 80.4f, 15.3f);
+		//console.DrawLine(10.0f, 61.0f, 80.0f, 61.0f);
+		//console.DrawLine(0.0f, 0.0f, 3.0f, 4.0f);
 
-		//console.DrawLine(50, 50, 60, 50);
-		//console.DrawLine(50, 50, 60, 60);
-		//console.DrawLine(50, 50, 50, 60);
-		//console.DrawLine(50, 50, 40, 60);
-		//console.DrawLine(50, 50, 40, 50);
-		//console.DrawLine(50, 50, 40, 40);
-		//console.DrawLine(50, 50, 50, 40);
-		//console.DrawLine(50, 50, 60, 40);
+		console.DrawLine(50, 50, 60, 50);
+		console.DrawLine(50, 50, 60, 60);
+		console.DrawLine(50, 50, 50, 60);
+		console.DrawLine(50, 50, 40, 60);
+		console.DrawLine(50, 50, 40, 50);
+		console.DrawLine(50, 50, 40, 40);
+		console.DrawLine(50, 50, 50, 40);
+		console.DrawLine(50, 50, 60, 40);
 
 		console.Render();
 	}
--- Source/Console.cpp
+++ Source/Console.cpp
@@ -16,17 +16,6 @@ Console::Console() :
 	screen = new char[width * height];
 	hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 	SetConsoleActiveScreenBuffer(hConsole);
-
-	//for (unsigned int i = 0; i < width; i++)
-	//	screen[i + 20 * 120] = '#';
-	//
-	//for (unsigned int i = 0; i < height; i++)
-	//	screen[20 + i * 120] = '#';
-	//
-	////screen[20 + 20 * 120] = 'a';
-	//
-	//screen[width * height - 1] = '\0';
-	//WriteConsoleOutputCharacter(hConsole, screen, width * height, { 0,0 }, &dwBytesWritten);
 }
 
 Console::~Console()
@@ -47,33 +36,65 @@ void Console::Clear()
 
 void Console::DrawPoint(float x, float y, char c)
 {
-	ASSERT(x >= 0 && x < width);
-	ASSERT(y >= 0 && y < height);
+	//ASSERT(x >= 0 && x < width);
+	//ASSERT(y >= 0 && y < height);
+
+	if (x < 0 || x >= width)
+		return;
+
+	if (y < 0 || y >= height)
+		return;
 
 	screen[(INT32)x + (INT32)y * width] = c;
 }
 
 void Console::DrawLine(float x1, float y1, float x2, float y2)
 {
-	//static const int intervals[] = { 0, 45, 90, 135, 180, 225, 270, 315, 360 };
-	static const int intervals[] = { -180, -135, -90, -45, 0, 45, 90, 135, 180 };
+	//static const int intervals[] = { -180, -135, -90, -45, 0, 45, 90, 135, 180 };
+	//
+	//float a = 180 * atan2(y2 - y1, x2 - x1) / PI;
+	//
+	//UINT8 idxmin = 0, idxmax = 8;
+	//
+	//while ((idxmax - idxmin) > 1)
+	//{
+	//	UINT8 mid = (idxmax + idxmin) / 2;
+	//
+	//	if (a >= intervals[mid])
+	//		idxmin = mid;
+	//	else
+	//		idxmax = mid;
+	//}
 
-	float a = 180 * atan2(y2 - y1, x2 - x1) / PI;
+	//float dx = x2 - x1;
+	//float dy = y2 - y1;
+	//
+	//float a = dy;
+	//float b = -dx;
+	//float c = -((a * x1) + (b * y1));
 
-	UINT8 idxmin = 0, idxmax = 8;
+	int X1 = x1;
+	int Y1 = y1;
+	int X2 = x2;
+	int Y2 = y2;
 
-	while ((idxmax - idxmin) > 1)
+	int DX = X2 - X1;
+	int DY = Y2 - Y1;
+
+	float r = (float)DY / (float)DX;
+
+	int X = X1;
+	int Y = Y1;
+
+	int sx = (0 < DX) - (DX < 0);
+
+	while (X != X2)
 	{
-		UINT8 mid = (idxmax + idxmin) / 2;
+		X += sx;
+		Y = Y1 + r * X;
 
-		if (a >= intervals[mid])
-			idxmin = mid;
-		else
-			idxmax = mid;
+		DrawPoint(X, Y);
 	}
-
-	DrawPoint(x1, y1);
-	DrawPoint(x2, y2);
 }
 
 void Console::DisplayMessage(const std::string & msg)
