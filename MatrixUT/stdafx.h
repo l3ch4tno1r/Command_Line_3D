2fc2c469db662cac62fb2d2e188167a1ae3f2f37
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-02-11 22:25:34 +0100

    Added unit test for Matrix

---
+++ MatrixUT/stdafx.h
@@ -0,0 +1,13 @@
+// stdafx.h : fichier Include pour les fichiers Include système standard,
+// ou les fichiers Include spécifiques aux projets qui sont utilisés fréquemment,
+// et sont rarement modifiés
+//
+
+#pragma once
+
+#include "targetver.h"
+
+// En-têtes pour CppUnitTest
+#include "CppUnitTest.h"
+
+// TODO: faites référence ici aux en-têtes supplémentaires nécessaires au programme
