39516c630f79d010b276808e33c11c6bfa998c32
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-02-11 23:45:26 +0100

    Matrix WIP

--- Matrix/Source/SMatrix.h
+++ Matrix/Source/SMatrix.h
@@ -15,9 +15,45 @@ namespace Matrix
 		T m_Matrix[L][C];
 
 	public:
+		/////////////////////////////////////
+		//-- Constructos and destructors --//
+		/////////////////////////////////////
 		SMatrix() :
 			m_Lines(L),
 			m_Columns(C)
 		{}
+
+		SMatrix(const T mat[L][C]) :
+			m_Lines(L),
+			m_Columns(C)
+		{
+			for (unsigned int i = 0; i < L; i++)
+				memcpy(m_Matrix[i], mat[i], C * sizeof(T));
+		}
+
+		SMatrix(const SMatrix& mat) :
+			SMatrix(mat.m_Matrix)
+		{}
+
+		///////////////////
+		//-- Accessors --//
+		///////////////////
+		unsigned int Lines() const
+		{
+			return m_Lines;
+		}
+
+		unsigned int Columns() const
+		{
+			return m_Columns;
+		}
+
+		T& operator()(unsigned int i, unsigned int j)
+		{
+			if (i >= L || j >= C)
+				throw std::out_of_range("Index out of range.");
+
+			return m_Matrix[i][j];
+		}
 	};
 }
