5de27a93009ceec9a3c4a7e5952a74eeba64fd63
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-02-15 20:09:37 +0100

    Update SqrSMatrix.h

--- Matrix/Source/Stack/SqrSMatrix.h
+++ Matrix/Source/Stack/SqrSMatrix.h
@@ -8,5 +8,48 @@ namespace Matrix
 	class SqrSMatrix : public SMatrix<T, LC, LC>
 	{
 	public:
+		/////////////////////////////////////
+		//-- Constructos and destructors --//
+		/////////////////////////////////////
+		SqrSMatrix() : SMatrix()
+		{}
+
+		SqrSMatrix(bool) : SMatrix()
+		{
+			for (uint i = 0; i < LC; i++)
+				for (uint j = 0; j < LC; j++)
+					m_Matrix[i][j] = (i == j ? T(1) : T(0));
+		}
+
+		SqrSMatrix(T value) : SMatrix(value)
+		{}
+
+		SqrSMatrix(const T mat[LC][LC]) : SMatrix(mat)
+		{}
+
+		SqrSMatrix(const SqrSMatrix& mat) : SMatrix(mat)
+		{}
+
+		/////////////////
+		//-- Methods --//
+		/////////////////
+		T Trace() const
+		{
+			T result(T(0));
+
+			for (uint i = 0; i < LC; i++)
+				result += m_Matrix[i][i];
+
+			return result;
+		}
+
+		////////////////////////
+		//-- Static Methods --//
+		////////////////////////
+		static const SqrSMatrix& Identity()
+		{
+			static SqrSMatrix id(true);
+			return id;
+		}
 	};
 }
