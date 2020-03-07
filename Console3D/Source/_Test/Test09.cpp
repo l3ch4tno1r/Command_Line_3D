4dfe4b6986a4f51a3ba8367aae9aac2f12315cd9
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-03-07 18:04:58 +0100

    Update Test09.cpp

--- Console3D/Source/_Test/Test09.cpp
+++ Console3D/Source/_Test/Test09.cpp
@@ -2,31 +2,38 @@
 
 #include <iostream>
 
+#include "Utilities\Utils.h"
+
+#include "Geometry\Geometry3D\HVector3D.h"
+
+const float cm_ScreenDist = 0.5f;
+
+void ScreenIntersect(HVector3D& a, HVector3D& b)
+{
+	if (sign(a.z - cm_ScreenDist) == sign(b.z - cm_ScreenDist))
+		return;
+
+	HVector3D& p1 = (a.z > 0.0f ? a : b);
+	HVector3D& p2 = (a.z > 0.0f ? b : a);
+
+	float dz = p2.z - p1.z;
+
+	if (std::abs(dz) < 0.001f)
+		return;
+
+	float k = (cm_ScreenDist - p2.z) / dz;
+
+	p2 = p2 + k * (p2 - p1);
+}
+
 int main()
 {
-	enum Test
-	{
-		Behind = -1,
-		Center =  1,
-		Top    =  2,
-		Bottom =  4,
-		Left   =  8,
-		Right  =  16
-	};
+	HVector3D a = { 2.0f, 0.0f, 0.0f };
+	HVector3D b = { 0.0f, 0.0f, 1.0f };
 
-	short a = 0;
+	ScreenIntersect(a, b);
 
-	a |= Test::Center;
-
-	std::cout << a << std::endl;
-
-	a |= Test::Left;
-
-	std::cout << a << std::endl;
-
-	a = Test::Top | Test::Right;
-
-	std::cout << a << std::endl;
+	std::cout << a << ", " << b << std::endl;
 
 	std::cin.get();
 }
