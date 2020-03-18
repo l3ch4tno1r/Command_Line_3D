8a040ddf4270175118b56731a89e8ca8dcdbdf5b
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-03-18 23:39:21 +0100

    Testing stb_image

---
+++ MultiPurposeTests/Source/_Test/Test14 - stb_image.cpp
@@ -0,0 +1,37 @@
+#ifdef TEST14
+
+#include <iostream>
+#include <string>
+
+#include "External\stb_image\stb_image.h" 
+
+int main()
+{
+	std::string filepath("Ressources/Test.png");
+
+	unsigned char* localbuffer;
+	int width;
+	int height;
+	int BPP;
+
+	localbuffer = stbi_load(filepath.c_str(), &width, &height, &BPP, STBI_grey);
+
+	if (stbi_failure_reason())
+		std::cout << stbi_failure_reason() << std::endl;
+
+	std::cout << width << ", " << height << ", " << BPP << std::endl;
+
+	for (unsigned int i = 0; i < width; i++)
+	{
+		for (unsigned int j = 0; j < height; j++)
+			std::cout << localbuffer[i + j * height];
+
+		std::cout << std::endl;
+	}
+
+	std::cin.get();
+
+	stbi_image_free(localbuffer);
+}
+
+#endif // TEST14
