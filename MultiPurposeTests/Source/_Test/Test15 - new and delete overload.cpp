d0c4c6bd7d3c8b9dd2f98a4c9476a07b493ee903
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-03-22 20:52:24 +0100

    Experiments

---
+++ MultiPurposeTests/Source/_Test/Test15 - new and delete overload.cpp
@@ -0,0 +1,72 @@
+#ifdef TEST15
+
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <stack>
+
+void* operator new(size_t size)
+{
+	std::cout << "Allocating " << size << " bytes" << std::endl;
+
+	return malloc(size);
+}
+
+void operator delete(void* ptr, size_t size)
+{
+	std::cout << "Deallocating " << size << " bytes" << std::endl;
+
+	free(ptr);
+}
+
+#define LOGADDRESS(X) stream << (int)&(X) << std::endl
+
+int main()
+{
+	{
+		std::queue<int> queue;
+		std::ofstream   stream("memory.csv", std::ios::out);
+
+		std::cout << "-- Pushing #1 --" << std::endl;
+
+		for (uint32_t i = 0; i < 100; i++)
+		{
+			std::cout << "Pushing " << i << std::endl;
+			queue.push(i);
+			LOGADDRESS(queue.front());
+		}
+
+		std::cout << "-- Poping #1 --" << std::endl;
+
+		while (queue.front() != 50)
+		{
+			std::cout << "Poping " << queue.front() << std::endl;
+			LOGADDRESS(queue.front());
+			queue.pop();
+		}
+
+		std::cout << "-- Pushing #2 --" << std::endl;
+
+		for (uint32_t i = 0; i < 100; i++)
+		{
+			std::cout << "Pushing " << i << std::endl;
+			queue.push(i);
+			LOGADDRESS(queue.front());
+		}
+
+		std::cout << "-- Poping #2 --" << std::endl;
+
+		while (!queue.empty())
+		{
+			std::cout << "Poping " << queue.front() << std::endl;
+			LOGADDRESS(queue.front());
+			queue.pop();
+		}
+
+		std::cout << "----------------" << std::endl;
+	}
+
+	std::cin.get();
+}
+
+#endif // TEST15
