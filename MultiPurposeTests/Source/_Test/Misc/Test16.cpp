5eb908d3dfdfbfe649edc7a4da379f5900b2d9ac
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-05-04 18:27:49 +0200

    Testing

--- MultiPurposeTests/Source/_Test/Misc/Test16.cpp
+++ MultiPurposeTests/Source/_Test/Misc/Test16.cpp
@@ -2,6 +2,22 @@
 #include <unordered_set>
 #include <utility>
 
+#define SEPARATOR(X) std::cout << "------- " << X << " -------" << std::endl
+
+void* operator new(size_t size)
+{
+	std::cout << "Allocating " << size << " bytes" << std::endl;
+
+	return malloc(size);
+}
+
+void operator delete(void* ptr, size_t size)
+{
+	std::cout << "Deallocating " << size << " bytes" << std::endl;
+
+	free(ptr);
+}
+
 using Edge = std::pair<unsigned int, unsigned int>;
 
 struct HashEdge
@@ -12,20 +28,44 @@ struct HashEdge
 	}
 };
 
+struct EqualEdge
+{
+	bool operator()(const Edge& e1, const Edge& e2) const
+	{
+		return (e1.first == e2.first  && e1.second == e2.second) ||
+			   (e1.first == e2.second && e1.second == e2.first);
+	}
+};
+
 int main()
 {
-	std::unordered_set<Edge, HashEdge> edges;
+	std::unordered_set<Edge, HashEdge, EqualEdge> edges;
+
+	SEPARATOR(1);
 
 	Edge edge1 = { 1, 2 };
 	Edge edge2 = { 2, 1 };
+	Edge edge3 = { 1, 3 };
 
+	std::cout << "edge1 == edge2 ? " << EqualEdge()(edge1, edge2) << std::endl;
+	std::cout << "edge1 == edge1 ? " << EqualEdge()(edge1, edge1) << std::endl;
+	std::cout << "edge1 == edge3 ? " << EqualEdge()(edge1, edge3) << std::endl;
+
+	SEPARATOR(2);
 	edges.insert(edge1);
+	SEPARATOR(3);
+	edges.insert(edge2);
+	SEPARATOR(4);
+	edges.insert(edge3);
+
+	SEPARATOR(5);
 
 	std::cout << edges.count(edge1) << std::endl;
 	std::cout << edges.count(edge2) << std::endl;
+	std::cout << edges.count(edge3) << std::endl;
 
-	if(edges.count(edge2) == 0)
-		edges.insert(edge2);
+	for (auto it = edges.begin(); it != edges.end(); it++)
+		std::cout << it->first << ", " << it->second << std::endl;
 
 	std::cin.get();
 }
