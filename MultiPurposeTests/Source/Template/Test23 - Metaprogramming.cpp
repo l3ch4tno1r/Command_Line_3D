9ec10582d39952ddc813d3b27045b3a669f39599
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-06-06 20:15:25 +0200

    Tests

--- MultiPurposeTests/Source/Template/Test23 - Metaprogramming.cpp
+++ MultiPurposeTests/Source/Template/Test23 - Metaprogramming.cpp
@@ -12,9 +12,68 @@ struct Factorielle<0>
 	enum { valeur = 1 };
 };
 
+/////////////////
+//-- Counter --//
+/////////////////
+
+template<class C>
+class Counter
+{
+private:
+	static int count;
+
+public:
+	Counter()
+	{
+		++count;
+	}
+
+	Counter(const Counter&)
+	{
+		++count;
+	}
+
+	static int Count()
+	{
+		return count;
+	}
+};
+
+template<class C>
+int Counter<C>::count = 0;
+
+class Test : public Counter<Test>
+{
+private:
+	int id;
+
+public:
+	Test() :
+		id(Counter<Test>::Count())
+	{
+		std::cout << "default ctor " << id << std::endl;
+	}
+
+	Test(const Test&) :
+		id(Counter<Test>::Count())
+	{
+		std::cout << "copy ctor " << id << std::endl;
+	}
+
+	Test(Test&&) :
+		id(Counter<Test>::Count())
+	{
+		std::cout << "move ctor " << id << std::endl;
+	}
+};
+
 int main()
 {
-	std::cout << "Factorielle 5 = " << Factorielle<5>::valeur << std::endl;
+	Test test1;
+	Test test2 = test1;
+	Test test3 = std::move(test1);
+
+	std::cout << Test::Count() << std::endl;
 
 	std::cin.get();
 }
