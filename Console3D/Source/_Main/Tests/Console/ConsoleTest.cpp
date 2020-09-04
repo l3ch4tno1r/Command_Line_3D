99528e213eaae83aa1884a0a411278eaade373f9
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-09-04 12:04:52 +0200

    Update ConsoleTest.cpp

--- Console3D/Source/_Main/Tests/Console/ConsoleTest.cpp
+++ Console3D/Source/_Main/Tests/Console/ConsoleTest.cpp
@@ -6,8 +6,16 @@
 #include "Utilities/Utils.h"
 #include "Utilities/Instrumentor.h"
 
+#include "Utilities/Angles.h"
+#include "Geometry/Geometry2D/Transform2D.h"
+#include "Geometry/Geometry2D/HVector2D.h"
+
 #include <iostream>
 #include <sstream>
+
+using namespace LCNMath::Geometry::Dim2;
+
+using Pixelf = HVector2Df;
 
 bool Console::Wait()
 {
@@ -31,6 +39,18 @@ void Console::Notify(bool run)
 	m_PauseCondition.notify_one();
 }
 
+void Console::DrawLine2(int x1, int y1, int x2, int y2, short c, short color)
+{
+	DrawPoint(x1, y1, 'X');
+	DrawPoint(x2, y2, 'X');
+
+	int dx = x2 - x1;
+	int dy = y2 - y1;
+
+
+}
+
+#if false
 void Console::MainThread()
 {
 	Instrumentor::Get().BeginSession("Test fill triangle");
@@ -120,5 +140,38 @@ void Console::MainThread()
 	FillTriangleRecursive({ p1, p2, p3 }, { TL, BR });
 	RENDER_AND_WAIT;
 	*/
+}
+#endif
+void Console::MainThread()
+{
+	float theta  = 0;
+	float aspeed = 6;            // degrès secondes
+	float dt     = 16.0f / 1000; // 16ms
+
+	Transform2Df frame = {
+		1.0f, 0.0f, (float)m_Width / 2,
+		0.0f, 1.0f, (float)m_Height / 2,
+		0.0f, 0.0f, 1.0f
+	};
+
+	Pixelf _p = { 0.0f, -30.0f };
+
+	while (Continue())
+	{
+		Clear();
+
+		Pixelf pix = frame.mat * _p.mat;
+
+		DrawLine(frame.Tx, frame.Ty, pix.x, pix.y);
+
+		theta += aspeed * dt;
+
+		frame.mat(0, 0) =  std::cos(TORAD(theta));
+		frame.mat(0, 1) = -std::sin(TORAD(theta));
+		frame.mat(1, 0) =  std::sin(TORAD(theta));
+		frame.mat(1, 1) =  std::cos(TORAD(theta));
+
+		Render();
+	}
 }
 #endif
