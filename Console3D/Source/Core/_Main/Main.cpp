b9d9fb7c7d55b99705054318306746db58121afe
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-10-30 20:48:22 +0100

    Exception handling

--- Console3D/Source/Core/_Main/Main.cpp
+++ Console3D/Source/Core/_Main/Main.cpp
@@ -4,5 +4,18 @@
 
 int main(int argc, char** argv)
 {
-	Application::Get().Run();
+	try
+	{
+		Application::Get().Run();
+
+		return EXIT_SUCCESS;
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "Critical error occured : " << e.what() << std::endl;
+
+		std::cin.get();
+
+		return EXIT_FAILURE;
+	}
 }
