9db069972ae7e65f6ea260ded9327f9bfe2c3f07
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-11-04 00:43:45 +0100

    WIP

--- Console3D/Source/Rendering/Camera.h
+++ Console3D/Source/Rendering/Camera.h
@@ -6,23 +6,17 @@ class Camera
 {
 	using Matrixf34 = LCNMath::Matrix::StaticMatrix::Matrix<float, 3, 4>;
 
+public:
+
+	Camera();
+
+	const Transform3D<float>& R0ToCamera() const { return m_R0ToCam; }
+
+	const Matrixf34& Projection() const { return m_Projection; }
+
 private:
 	Transform3D<float> m_R0ToCam;
 
 	float     m_Focal;
 	Matrixf34 m_Projection;
-
-public:
-
-	Camera();
-
-	inline const Transform3D<float>& R0ToCamera() const
-	{
-		return m_R0ToCam;
-	}
-
-	inline const Matrixf34& Projection() const
-	{
-		return m_Projection;
-	}
 };
