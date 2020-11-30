3ce2ed6a2fd7ab52676535f78369d34501e3c119
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-11-30 21:03:07 +0100

    Matrix 2.0 integration #3

--- Console3D/Source/Rendering/Camera.h
+++ Console3D/Source/Rendering/Camera.h
@@ -1,21 +1,21 @@
 #pragma once
 
-#include "LCN_Math/Source/Geometry/Geometry3D/Transform3D.h"
+#include "LCN_Math/Source/Geometry/Transform.h"
 
 class Camera
 {
-	using Matrixf34 = LCNMath::Matrix::StaticMatrix::Matrix<float, 3, 4>;
+	using Matrixf34 = StaticMatrix<float, 3, 4>;
 
 public:
 
 	Camera();
 
-	const Transform3D<float>& R0ToCamera() const { return m_R0ToCam; }
+	const Transform3Df& R0ToCamera() const { return m_R0ToCam; }
 
 	const Matrixf34& Projection() const { return m_Projection; }
 
 private:
-	Transform3D<float> m_R0ToCam;
+	Transform3Df m_R0ToCam;
 
 	float     m_Focal;
 	Matrixf34 m_Projection;
