81418071a97083df0aee4f9b3d6bd7fb2ebe6b45
l3ch4tno1r <arnaud.granier34@gmail.com>
2021-03-06 18:51:21 +0100

    Add EntryPoint.h

---
+++ Console3D/Source/Core/EntryPoint.h
@@ -0,0 +1,27 @@
+#pragma once
+
+int main(int argc, char** argv)
+{
+	try
+	{
+		Application::Get().Run();
+
+		return EXIT_SUCCESS;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Critical error occured : " << e.what() << std::endl;
+
+		std::cin.get();
+
+		return EXIT_FAILURE;
+	}
+	catch (...)
+	{
+		std::cerr << "Unknown error occured" << std::endl;
+
+		std::cin.get();
+
+		return EXIT_FAILURE;
+	}
+}
--- Console3DSandbox/Source/Main.cpp
+++ Console3DSandbox/Source/Main.cpp
@@ -9,28 +9,4 @@
 
 using namespace LCN::Core;
 
-int main(int argc, char** argv)
-{
-	try
-	{
-		Application::Get().Run();
-
-		return EXIT_SUCCESS;
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << "Critical error occured : " << e.what() << std::endl;
-
-		std::cin.get();
-
-		return EXIT_FAILURE;
-	}
-	catch (...)
-	{
-		std::cerr << "Unknown error occured" << std::endl;
-
-		std::cin.get();
-
-		return EXIT_FAILURE;
-	}
-}
+#include "Console3D/Source/Core/EntryPoint.h"
