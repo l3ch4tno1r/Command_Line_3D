a059a3db3940b523b7b8ca5a75ea33faf63d3e71
l3ch4tno1r <arnaud.granier34@gmail.com>
2021-03-16 00:13:51 +0100

    Collisions WIP

--- Console3DTests/Source/Collisions/Test01.cpp
+++ Console3DTests/Source/Collisions/Test01.cpp
@@ -4,7 +4,11 @@
 #include <LCN_Collisions/Source/Shapes/AABB.h>
 #include <LCN_Collisions/Source/Shapes/Point.h>
 
-#include <LCN_Collisions/Source/Collisions/CollisionAlgorithms.h>
+#include <LCN_Collisions/Source/Collisions/CollisionFunctions.h>
+
+using namespace LCN;
+
+#define LOG(X) std::cout << #X << " = " << X << std::endl;
 
 int main()
 {
@@ -12,13 +16,43 @@ int main()
 		{ 1, 2, 1 },
 		{ 1, 1, 0 });
 
-	LCN::AABB2Df aabb(
+	LCN::AABB2Df aabb1(
 		{ 0, 3, 1 },
 		4, 3);
 
-	LCN::Point2Df pt;
+	LCN::Point2Df pt1 = {  1,  1, 1 };
+	LCN::Point2Df pt2 = { -1,  1, 1 };
+	LCN::Point2Df pt3 = { -1,  4, 1 };
+	LCN::Point2Df pt4 = {  1,  4, 1 };
+	LCN::Point2Df pt5 = {  5,  4, 1 };
+	LCN::Point2Df pt6 = {  5,  2, 1 };
+	LCN::Point2Df pt7 = {  5, -1, 1 };
+	LCN::Point2Df pt8 = {  1, -1, 1 };
+	LCN::Point2Df pt9 = { -1, -1, 1 };
 
-	auto result = LCN::Collision<LCN::CollisionPolicy::DetectionOnly>(aabb, pt);
+	LOG(DetectCollision(aabb1, pt1));
+	LOG(DetectCollision(aabb1, pt2));
+	LOG(DetectCollision(aabb1, pt3));
+	LOG(DetectCollision(aabb1, pt4));
+	LOG(DetectCollision(aabb1, pt5));
+	LOG(DetectCollision(aabb1, pt6));
+	LOG(DetectCollision(aabb1, pt7));
+	LOG(DetectCollision(aabb1, pt8));
+	LOG(DetectCollision(aabb1, pt9));
+
+	LCN::AABB2Df aabb2 = {
+		{ 2, 4, 1 },
+		4, 2
+	};
+
+	LCN::AABB2Df aabb3 = {
+		{ 5, 1, 1 },
+		4, 3
+	};
+
+	LOG(DetectCollision(aabb1, aabb2));
+	LOG(DetectCollision(aabb1, aabb3));
+	LOG(DetectCollision(aabb2, aabb3));
 
 	std::cin.get();
 }
