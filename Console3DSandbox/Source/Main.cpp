fcc54d30c25db21bae1b19351a12cf04a954c9d4
l3ch4tno1r <arnaud.granier34@gmail.com>
2021-03-28 23:41:24 +0200

    Clean up

--- Console3DSandbox/Source/Main.cpp
+++ Console3DSandbox/Source/Main.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
 
-//#include "TestApps/AppTest01.h"
-//#include "Misc/AppTestEvent.h"
-//#include "Misc/AppTestBasicPaint.h"
-//#include "Misc/RTAppTest.h"
-#include "Misc/AppTestDisplayImage.h"
-//#include "Misc/AppTestWidget.h"
+#include "Console3D/Source/Core/Application.h"
 
 #include "Console3D/Source/Core/EntryPoint.h"
