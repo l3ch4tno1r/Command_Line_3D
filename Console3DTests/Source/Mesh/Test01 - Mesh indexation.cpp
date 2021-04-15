37d7db27481676bd153406b0fa55d42e9f88c877
l3ch4tno1r <arnaud.granier34@gmail.com>
2021-04-15 23:31:01 +0200

    Testing

---
+++ Console3DTests/Source/Mesh/Test01 - Mesh indexation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <Console3D/Source/RessourceManagement/OBJReader.h>
+
+int main()
+{
+	LCN::Ressource::Model3D   model;
+	LCN::Ressource::OBJReader reader;
+
+	std::cin.get();
+}
